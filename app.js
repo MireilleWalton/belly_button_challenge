a4a324905030cd2ac0dd68f4f1cd0e715b05c48e
VioletRogue12 <mmj649@gmail.com>
2023-09-02 11:04:41 +1000

    woe, still getting nowhere

--- app.js
+++ app.js
@@ -1,31 +1,59 @@
-const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
+const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
-d3.json(url).then(function (samples) {
-    console.log(samples)});
+function init();
+d3.json(url).then(function (data) {
+  const bb_data = data.samples;
+  console.log(bb_data);
 
-//
+  let dropDownList = bb_data.map(row => row.id);
+  let top10_otu_id = bb_data.map(row => row.otu_ids.slice(0, 10));
+  let top10_otu_vals = bb_data.map(row => row.sample_values.slice(0, 10));
 
-  let dropDownList = Object.values(samples.id);
-  console.log(bb_data.id)
-  let top10_otu_id = Object.values(samples.otu_ids);
-  let top10_otu_vals = Object.values(samples.sample_values);
+  data1 = [{
+    x: top10_otu_vals[0], 
+    y: top10_otu_id[0],   
+    type: "bar",
+    orientation: "h",     
+    title: `Top 10 OTUs by Individual: {$top10_otu_id}`
+  }];
 
-// 
+  Plotly.newPlot("bar", data1);
 
-function init(){
-    data1 = [{
-        x: samples.map(row => row.top10_otu_vals.slice[0,10]),
-        y: samples.map(row => row.top10_otu_id.slice[0,10]),
-        type: "bar",
-        labels: dropDownList,
-        title: "Top 10 OTUs by Individual"}];
+d3.selectAll("#selDataset").on("onchange", updatePlotly);
+//create call function to activate change in drop down menue 
+function updatePlotly(){
+  let dropDownList = d3.select("#selDataset");
+  let dataset = dropDownList.property("#selDataset");
+
+  //initialise x and y arrays
+
+  if (dataset === "selDataset"){
+      x = top10_otu_vals[0]
+      y = top10_otu_id[0]};
+  }
+init()
+
+// function init(){
+
+// const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
     
-   Plotly.newPlot("bar", data1)
-};
-
-
-
-// let id = data.map(function (row){
+// d3.json(url).then(function (samples) {
+//   console.log(samples);});    
+//   //
+// let dropDownList = Object.values(samples.id);
+// let top10_otu_id = Object.values(samples.otu_ids);
+// let top10_otu_vals = Object.values(samples.sample_values);    
+// //     // 
+//   data1 = [{
+//     x: samples.map(row => row.top10_otu_vals.slice[0,10]),
+//     y: samples.map(row => row.top10_otu_id.slice[0,10]),
+//     type: "bar",
+//     labels: dropDownList,
+//     title: "Top 10 OTUs by Individual"}];
+        
+//   Plotly.newPlot("bar", data1)
+// //     // }
+// // let id = data.map(function (row){
 //     return row.samples.id
 //   });
 
@@ -72,3 +100,27 @@ function init(){
 // };
 
 // init()
+// function init(){
+
+
+
+//        let data = samples.map(function(item, index){
+//           return `data ${idex} : ${item}`;
+//        });
+// //   //
+  
+//     let dropDownList = Object.values(samples.id);
+//     console.log(bb_data.id)
+//     let top10_otu_id = Object.values(samples.otu_ids);
+//     let top10_otu_vals = Object.values(samples.sample_values);
+  
+//   // 
+//       data1 = [{
+//           x: samples.map(row => row.top10_otu_vals.slice[0,10]),
+//           y: samples.map(row => row.top10_otu_id.slice[0,10]),
+//           type: "bar",
+//           labels: dropDownList,
+//           title: "Top 10 OTUs by Individual"}];
+      
+//      Plotly.newPlot("bar", data1)
+//   };
